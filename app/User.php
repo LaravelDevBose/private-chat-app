c8e425a6dae98560f55a608d9719cd2187ef910f
LaravelDevBose <laravel.devbose@gmail.com>
2020-09-28 03:56:53 +0600

    Session,Chat,Message model and migration created

--- app/User.php
+++ app/User.php
@@ -10,6 +10,8 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $table= 'users';
+    protected $primaryKey='user_id';
     /**
      * The attributes that are mass assignable.
      *
